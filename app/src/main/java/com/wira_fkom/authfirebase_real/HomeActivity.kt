090644bce22b8995c933266466da8fe56847943b
Wira07 <wiralodrasaputra07@gmail.com>
2024-05-21 01:12:34 +0700

    update Logic LoginActivity, RegisterActivity dan HomeActivity

--- app/src/main/java/com/wira_fkom/authfirebase_real/HomeActivity.kt
+++ app/src/main/java/com/wira_fkom/authfirebase_real/HomeActivity.kt
@@ -1,11 +1,24 @@
 package com.wira_fkom.authfirebase_real
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.google.firebase.auth.FirebaseAuth
+import com.wira_fkom.authfirebase_real.databinding.ActivityHomeBinding
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+        binding.btnLogout.setOnClickListener {
+            auth.signOut()
+            Intent(this@HomeActivity, LoginActivity::class.java).also {
+                it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(it)
+            }
+        }
     }
 }
